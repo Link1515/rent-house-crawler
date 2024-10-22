909be45e2c115e1150c44a61ead74ef7d1752c29
Link1515 <h26281660@gmail.com>
2024-10-22 11:38:48 +0800

    create createHouseTable script

---
+++ script/createHousesTable.php
@@ -0,0 +1,17 @@
+<?php
+
+require_once __DIR__ . '/../bootstrap.php';
+
+use Link1515\RentHouseCrawler\DB;
+use Link1515\RentHouseCrawler\Repositories\HouseRepository;
+
+try {
+    $pdo = DB::getPDO();
+
+    $houseRepository = new HouseRepository($pdo);
+    $houseRepository->createHousesTable();
+
+    echo "Table 'houses' created successfully.";
+} catch (PDOException $e) {
+    echo 'Error: ' . $e->getMessage();
+}
