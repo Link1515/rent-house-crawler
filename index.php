b088f3af3138b81012403443e261dfbb5b1651ad
Link1515 <h26281660@gmail.com>
2024-10-25 20:48:57 +0800

    add index page message

--- index.php
+++ index.php
@@ -1,35 +1,3 @@
 <?php
 
-require_once __DIR__ . '/bootstrap.php';
-
-use Link1515\RentHouseCrawler\DB;
-use Link1515\RentHouseCrawler\Repositories\HouseRepository;
-use Link1515\RentHouseCrawler\Services\CrawlHouseService;
-use Link1515\RentHouseCrawler\Services\MessageServices\ConsoleMessageService;
-use Link1515\RentHouseCrawler\Services\MessageServices\DiscordMessageService;
-use Link1515\RentHouseCrawler\Utils\UrlUtils;
-
-$queryParams = [
-    'region'  => 1,
-    'metro'   => 162,
-    'kind'    => 2,
-    'price'   => '5000_10000',
-    'sort'    => 'posttime_desc',
-    'station' => 4232
-];
-$url = UrlUtils::getHouseListUrl($queryParams);
-
-$houseRepository = new HouseRepository(DB::getPDO(), 'houses');
-// $messageService  = new DiscordMessageService($_ENV['DISCORD_WEBHOOK']);
-$messageService = new ConsoleMessageService();
-
-$crawlHouseService = new CrawlHouseService(
-    $houseRepository,
-    $messageService,
-    $url,
-    [
-        'excludeWomanOnly' => true,
-    ]
-);
-
-$crawlHouseService->crawl();
+echo 'Hello World!';
---
+++ run.php
@@ -0,0 +1,33 @@
+<?php
+
+require_once __DIR__ . '/bootstrap.php';
+
+use Link1515\RentHouseCrawler\DB;
+use Link1515\RentHouseCrawler\Repositories\HouseRepository;
+use Link1515\RentHouseCrawler\Services\CrawlHouseService;
+use Link1515\RentHouseCrawler\Services\MessageServices\DiscordMessageService;
+use Link1515\RentHouseCrawler\Utils\UrlUtils;
+
+$queryParams = [
+    'region'  => 1,
+    'metro'   => 162,
+    'kind'    => 2,
+    'price'   => '5000_10000',
+    'sort'    => 'posttime_desc',
+    'station' => 4232
+];
+$url = UrlUtils::getHouseListUrl($queryParams);
+
+$houseRepository = new HouseRepository(DB::getPDO(), 'houses');
+$messageService  = new DiscordMessageService($_ENV['DISCORD_WEBHOOK']);
+
+$crawlHouseService = new CrawlHouseService(
+    $houseRepository,
+    $messageService,
+    $url,
+    [
+        'excludeWomanOnly' => true,
+    ]
+);
+
+$crawlHouseService->crawl();
