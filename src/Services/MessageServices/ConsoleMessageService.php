e17c4ac4f0b77d74acb279ce355efe0da01f9c90
Link1515 <h26281660@gmail.com>
2024-10-25 11:14:11 +0800

    create ConsoleMessageService

---
+++ src/Services/MessageServices/ConsoleMessageService.php
@@ -0,0 +1,38 @@
+<?php
+
+namespace Link1515\RentHouseCrawler\Services\MessageServices;
+
+use Link1515\RentHouseCrawler\Entities\House;
+
+class ConsoleMessageService implements MessageServiceInterface
+{
+    public function sendHouseMessage(House $house, string $description, array $images): void
+    {
+        $images = join("\n", $images);
+
+        $message = <<<Message
+        新通知：
+
+        {$house->title}
+
+        房租: {$house->price}
+        位置: {$house->address}
+        樓層: {$house->floor}
+        發布者: {$house->poster}
+
+        {$description}
+
+        {$house->getLink()}
+
+        {$images}
+        \n
+        Message;
+
+        $this->sendMessage($message);
+    }
+
+    public function sendMessage(string $message): void
+    {
+        echo $message;
+    }
+}
